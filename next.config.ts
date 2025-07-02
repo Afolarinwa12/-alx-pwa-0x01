aa4c2d33b959a8176fa02268f3f98f9230fec56c
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-07-02 22:48:29 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+/** import type { NextConfig } from "next";*/
 
-const nextConfig: NextConfig = {
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  }
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig
+})
