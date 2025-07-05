8cba70430bcdbf747f328e31b06e8ad7a1f9210f
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-07-05 22:02:41 +0100

    complete tasks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
+      <body className="antialised">
         <Main />
         <NextScript />
       </body>
